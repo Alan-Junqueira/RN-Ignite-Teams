0b31be96a9a2f18f9396c797c03c3124dc654c5a
Alan Junqueira <contato.alanjunqueira@gmail.com>
2023-05-14 22:00:09 -0300

    style: change Input placeholder color

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,6 @@
 import { TextInputProps } from "react-native";
+import { useTheme } from 'styled-components/native'
+
 import { InputContainer } from "./styled";
 
 type TInput = TextInputProps & {
@@ -6,8 +8,11 @@ type TInput = TextInputProps & {
 
 
 export const Input = ({ ...props }: TInput) => {
+  const { COLORS } = useTheme()
+
   return (
     <InputContainer
+      placeholderTextColor={COLORS.GRAY_300}
       {...props}
     />
   )
